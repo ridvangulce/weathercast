0ed24298aa7baaea8788aed024e5290e04a51017
ridvangulce <ridvangulce@gmail.com>
2023-05-19 08:16:40 +0300

    +fixed bugs, better search, better icons

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,81 @@
 "use client";
 import { useState } from "react"
 import { BsSearch } from "react-icons/bs"
+import Image from "next/image";
 import axios from "axios";
-
+import Weather from "@/components/Weather";
+import Spinner from "../public/spinner.gif"
 export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&lang=tr&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const [error, setError] = useState(null)
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=tr&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
   const fetchWeather = (e) => {
     e.preventDefault();
     setLoading(true);
-    axios.get(url).then((res) => {
-      setWeather(res.data);
-      console.log(res.data);
-      setCity("");
-      setLoading(false)
-    })
+    axios.get(url)
+      .then((res) => {
+        setWeather(res.data);
+        setCity("");
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error); // Hata durumunu konsola yazdırabilirsiniz.
+      
+        setError("Weather data couldn't be retrieved. Please try again." );
+      });
+      setError(null)
   }
 
+
+
   return (
-    <div>
-      <br />
-      <button onClick={fetchWeather}>Fetch Data</button>
+    <div className="overflow-hidden ">
+
+      {/* OVERLAY!*/}
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
+
+      {/* BACKGROUND IMAGE*/}
+
+      <Image src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1075&q=80"
+        layout="fill"
+        alt="image/jpeg"
+        className="object-cover"
+      />
+      {/* SEARCH*/}
+      <div className="relative flex justify-between items-center max-w-[500px] m-auto pt-4 text-white z-10">
+        <form className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-400 rounded-2xl" onSubmit={fetchWeather}>
+          <div>
+            <input className="w-full bg-transparent border-none text-white focus:outline-none text-2xl"
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
+              type="text"
+              placeholder="Search City"
+            />
+          </div>
+          <div>
+            <button onClick={fetchWeather}><BsSearch size={30} /></button>
+          </div>
+        </form>
+      </div>
+      {/* WEATHER*/}
+      {loading ? (
+        <div className="relative">
+          <Image className="w-auto mt-44 m-auto" src={Spinner} alt="loading..." />
+        </div>
+      ) : (
+        <div className="items-center justify-center flex" >
+          {error ? (
+            <p className=" relative border rounded-3xl bg-gradient-to-r from-red-400 to-black p-10 items-center justify-center mt-20 text-white font-bold text-2xl text-center">{error}</p>
+          ) : (
+            weather.main && <Weather data={weather} />
+          )}
+        </div>
+      )}
+
     </div>
   )
 }
---
+++ components/Weather/index.jsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Weather = ({ data }) => {
+
+    return (
+        <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[70vh] m-auto p-4 text-gray-300 z-10">
+            <div className="relative flex gap-14 justify-between pt-12">
+                <div className='flex flex-col items-center'>
+                    <Image
+                        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                        width="150"
+                        height="150"
+                        alt='/'
+                    />
+                    <p className='font-bold font-sans mb-10 ml-10 text-6xl'>{data.weather[0].main}</p>
+                </div>
+                <p className=' font-bold text-9xl' >{data.main.temp.toFixed(0)}&#176;C</p>
+            </div>
+            <div className='bg-black/50 relative rounded-md p-8  h-64 items-center justify-center '>
+                <p className="text-2xl font-serif font-bold text-center pb-6">Weather in {data.name}</p>
+                <div className="flex justify-between text-center">
+                    <div className="">
+                        <p className="font-bold text-2xl">{data.main.feels_like.toFixed(0)}&#176;C</p>
+                        <p className="text-xl">Feels Like</p>
+                    </div>
+                    <div className="">
+                        <p className="font-bold text-2xl">{data.main.humidity}%</p>
+                        <p className="text-xl">Humidity</p>
+                    </div>
+                    <div className="">
+                        <p className="font-bold text-2xl">{data.wind.speed.toFixed(0)}MPH</p>
+                        <p className="text-xl">Winds</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Weather
