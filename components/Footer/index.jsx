f46434a8eeb96ea7b7d60a368200449aa2ccd1b0
ridvangulce <ridvangulce@gmail.com>
2023-05-20 06:58:54 +0300

    +fixed responsive footer bug

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 left-0 right-0 items-center flex justify-center '>
+    <footer className='absolute bottom-0 left-0 right-0 items-center flex justify-center bg-gradient-to-t from-black/90 to-transparent rounded-full '>
       <div className='items-center justify-center '>
-        <p className='bg-gradient-to-r from-red-900 to-black font-bold border-2 rounded-full text-center text-white border-gray-300 p-3'>Developed by Rıdvan Gulce</p>
+        <p className='bg-gradient-to-r from-red-900 to-black font-mono font-semibold text-xs md:text-lg md:font-bold border-2 rounded-full text-center text-white border-gray-300 p-2 md:p-3'>Developed by Rıdvan Gulce</p>
       </div>
     </footer>
   )
