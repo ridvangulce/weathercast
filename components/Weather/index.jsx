fa3de2e1b84c7bc909f4d400bf7631890b7eb0c5
ridvangulce <ridvangulce@gmail.com>
2023-05-20 06:46:03 +0300

    +responsive design

--- components/Weather/index.jsx
+++ components/Weather/index.jsx
@@ -5,7 +5,7 @@ const Weather = ({ data }) => {
 
     return (
         <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[70vh] m-auto p-4 text-gray-300 z-10">
-            <div className="relative flex gap-14 justify-between pt-12">
+            <div className="relative flex md:gap-14 justify-around pr-12 pt-12 ">
                 <div className='flex flex-col items-center'>
                     <Image
                         src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
@@ -15,7 +15,7 @@ const Weather = ({ data }) => {
                     />
                     <p className='font-bold font-sans mb-10 ml-10 text-6xl'>{data.weather[0].main}</p>
                 </div>
-                <p className=' font-bold text-9xl' >{data.main.temp.toFixed(0)}&#176;C</p>
+                <p className=' font-bold pt-10 text-7xl md:text-9xl' >{data.main.temp.toFixed(0)}&#176;C</p>
             </div>
             <div className='bg-black/50 relative rounded-md p-8  h-64 items-center justify-center '>
                 <p className="text-2xl font-serif font-bold text-center pb-6">Weather in {data.name}</p>
