013e8b6af11b9aba48cd7df75c01e5eea3a8571a
ridvangulce <ridvangulce@gmail.com>
2023-05-20 06:23:51 +0300

    +added footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='absolute bottom-0 left-0 right-0 items-center flex justify-center '>
+      <div className='items-center justify-center '>
+        <p className='bg-gradient-to-r from-red-900 to-black font-bold border-2 rounded-full text-center text-white border-gray-300 p-3'>Developed by Rıdvan Gulce</p>
+      </div>
+    </footer>
   )
 }
 
--- components/Weather/index.jsx
+++ components/Weather/index.jsx
@@ -34,6 +34,7 @@ const Weather = ({ data }) => {
                     </div>
                 </div>
             </div>
+           
         </div>
 
     )
